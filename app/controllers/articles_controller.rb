70fd0ea7961eb4fc6fafbf0bc0944ce430299084
Kyle <kyle.leeyaw@gmail.com>
2018-04-13 15:09:18 -0400

    added, edit, update, show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,40 +1,50 @@
-class ArticlesController < ApplicationController 
-   
-  def index
-    
-  end
-
-  def new 
-    @article = Article.new
-
-  end 
- 
-  def create 
-    @article = Article.new(article_params)              
-    
-    if @article.save
-      flash[:notice] = "Article was successfully saved"
-      redirect_to articles_path(@article)
-    else
-      
-      render 'new'     #can also write as :new
-    end
-  end
+class ArticlesController < ApplicationController
   
-  def show 
-    
+    def new #handled by create
+      @article = Article.new
+    end 
+  
+  def edit
     @article = Article.find(params[:id])
-    
   end 
+
+
+#new send the article params to create. calls the private method 
+    def create 
+        @article = Article.new(article_params)
+       if @article.save
+        flash[:notice] = "Article was succssfully created"
+        redirect_to articles_path(@article)
+       else
+        render 'new'  #can also write as :new
+       end
+     end
+
   
+    def show
+      @article = Article.find(params[:id])
+    end 
+  
+    def index
+      @articles = Article.all
+    end 
+  
+    def update(article_params) #handled by edit 
+      @article = Article.find(params[:id])
+      if @article.update
+      flash[:notice] = "Article was successfuly updated"
+      redirect_to article_path(@article)
+      else 
+        render 'edit'
+      end
+    end
   
   private 
   
-    def article_params
-      
-      params.require(:article).permit(:title, :description)
-      
-    end 
-  
-  
-end 
+  #white listed the params
+    def article_params  
+
+     params.require(:article).permit(:title, :description)
+    end
+
+end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,6 @@
-class Article <  ActiveRecord::Base
+class Article < ActiveRecord::Base
   
-  #insures there is a title in the datate base befoew a save 
-  validates :title, presence: true, length: {minimum: 3, maximum: 50}  #number of chars
+  validates :title, presence: true, length: {minimum: 3, maximum: 50}
   validates :description, presence: true, length: {minimum: 10, maximum: 300}
-  
   
 end 
